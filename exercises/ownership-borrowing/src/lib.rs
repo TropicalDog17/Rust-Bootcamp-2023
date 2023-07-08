64c5f47a18d3236b20c5bd294887780bf48c862f
Tuan Tran <tropicaldog17@gmail.com>
2023-07-08 16:20:06 +0700

    Finish exercise 2

--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -4,7 +4,7 @@ fn exercise1() {
     // Use as many approaches as you can to make it work
     let x = String::from("hello, world");
     let y = x;
-    let z = x;
+    let z = &y;
 }
 
 // Exercise 2
@@ -17,8 +17,9 @@ fn exercise2() {
     println!("{}", s2);
 }
 // Only modify the code below!
-fn take_ownership(s: String) {
+fn take_ownership(s: String) -> String {
     println!("{}", s);
+    s
 }
 
 // Exercise 3
@@ -26,9 +27,8 @@ fn take_ownership(s: String) {
 // Dont care about logic
 fn exercise3() {
     let values: Vec<f64> = vec![
-        2817.42, 2162.17, 3756.57, 2817.42, -2817.42, 946.9, 2817.42, 964.42, 795.43, 3756.57,
-        139.34, 903.58, -3756.57, 939.14, 828.04, 1120.04, 604.03, 3354.74, 2748.06, 1470.8,
-        4695.71, 71.11, 2391.48, 331.29, 1214.69, 863.52, 7810.01,
+        2817.42, 2162.17, 3756.57, 2817.42, -2817.42, 946.9, 2817.42, 964.42, 795.43, 3756.57, 139.34, 903.58, -3756.57, 939.14, 828.04, 1120.04, 604.03,
+        3354.74, 2748.06, 1470.8, 4695.71, 71.11, 2391.48, 331.29, 1214.69, 863.52, 7810.01,
     ];
 
     let values_number = values.len();
@@ -41,8 +41,8 @@ fn exercise3() {
         let mut addition: f64 = 0.0;
 
         // Sumar valores en additions
-        for element_index in additions {
-            let addition_aux = values[element_index];
+        for element_index in &additions {
+            let addition_aux = values[*element_index];
             addition = addition_aux + addition;
         }
     }
@@ -50,10 +50,9 @@ fn exercise3() {
 
 // Exercise 4
 // Make it compile
-fn exercise4(value: u32) -> &'static str {
+fn exercise4(value: u32) -> String {
     let str_value = value.to_string(); // Convert u32 to String
-    let str_ref: &str = &str_value; // Obtain a reference to the String
-    str_ref // Return the reference to the String
+    str_value
 }
 
 // Exercise 5
@@ -65,11 +64,12 @@ fn exercise5() {
     let key = 3;
 
     let res = match my_map.get(&key) {
-        Some(child) => child,
+        Some(child) => child.to_owned(),
         None => {
             let value = "3.0".to_string();
-            my_map.insert(key, value);
-            &value // HERE IT FAILS
+            my_map.insert(key, value.clone());
+            // HERE IT FAILS
+            value
         }
     };
 
@@ -82,14 +82,21 @@ fn exercise5() {
 use std::io;
 
 fn exercise6() {
-    let mut prev_key: &str = "";
+    let mut prev_key: String = String::new();
 
     for line in io::stdin().lines() {
-        let s = line.unwrap();
+        // let s = line.unwrap();
 
+        // let data: Vec<&str> = s.split("\t").collect();
+        // if prev_key.len() == 0 {
+        //     prev_key = data[0];
+        // }
+
+        let s = line.unwrap();
         let data: Vec<&str> = s.split("\t").collect();
+        let key = data[0];
         if prev_key.len() == 0 {
-            prev_key = data[0];
+            prev_key = key.to_owned();
         }
     }
 }
@@ -98,8 +105,8 @@ fn exercise6() {
 // Make it compile
 fn exercise7() {
     let mut v: Vec<&str> = Vec::new();
+    let chars = [b'x', b'y', b'z'];
     {
-        let chars = [b'x', b'y', b'z'];
         let s: &str = std::str::from_utf8(&chars).unwrap();
         v.push(&s);
     }
@@ -109,14 +116,12 @@ fn exercise7() {
 // Exercise 8
 // Make it compile
 fn exercise8() {
-    let mut accounting = vec!["Alice", "Ben"];
-    
+    let mut accounting = vec!["Alice".to_owned(), "Ben".to_owned()];
+
     loop {
         let mut add_input = String::from("");
 
-        io::stdin()
-            .read_line(&mut add_input)
-            .expect("Failed to read line");
+        io::stdin().read_line(&mut add_input).expect("Failed to read line");
 
         let add_vec: Vec<&str> = add_input.trim()[..].split_whitespace().collect();
 
@@ -126,6 +131,6 @@ fn exercise8() {
         }
 
         let person = add_vec[0];
-        accounting.push(person);
+        accounting.push(person.to_owned());
     }
 }
