764f3fd44c139519ffc024f5f358dd181e936869
Tuan Tran <tropicaldog17@gmail.com>
2023-07-18 18:53:02 +0700

    finish exercise 5

--- exercises/error-handling/src/lib.rs
+++ exercises/error-handling/src/lib.rs
@@ -2,22 +2,27 @@
 // Make it compile in unit test
 // Run tests
 // Hint: Convert Option to Result
-fn generate_nametag_text(name: String) -> Option<String> {
+fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        None
+        Err("`name` was empty; it must be nonempty.".into())
     } else {
-        Some(format!("Hi! My name is {}", name))
+        Ok(format!("Hi! My name is {}", name))
     }
 }
+use core::num;
+use std::str::FromStr;
 // Exercise 2
 // Make it compile in unit test
 // Run tests
 // Hint: &str to integer conversion by using parse method and return Result
-use std::num::ParseIntError;
-
+use std::{
+    io::{self, Error},
+    num::ParseIntError,
+};
 fn parse_number(s: &str) -> Result<i32, ParseIntError> {
-    todo!()
+    let num = s.parse::<i32>()?;
+    Ok(num)
 }
 
 // Exercise 3
@@ -36,6 +41,11 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
+        let value = match value {
+            i if i < 0 => return Err(CreationError::Negative),
+            i if i == 0 => return Err(CreationError::Zero),
+            i => i,
+        };
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
@@ -47,10 +57,7 @@ mod tests {
     /// Test for exercise 1
     #[test]
     fn exercise1_should_work() {
-        assert_eq!(
-            generate_nametag_text("Beyoncé".into()),
-            Ok("Hi! My name is Beyoncé".into())
-        );
+        assert_eq!(generate_nametag_text("Beyoncé".into()), Ok("Hi! My name is Beyoncé".into()));
 
         assert_eq!(
             generate_nametag_text("".into()),
@@ -63,20 +70,14 @@ mod tests {
     #[test]
     fn exercise2_should_work() {
         assert_eq!(parse_number("42"), Ok(42));
-        assert_eq!(
-            parse_number("invalid"),
-            Err("invalid digit found in string".parse().unwrap())
-        );
+        assert_eq!(parse_number("invalid").unwrap_err().to_string(), "invalid digit found in string");
     }
 
     /// Test for exercise 3
     #[test]
     fn exercise3_should_work() {
         assert!(PositiveNonzeroInteger::new(10).is_ok());
-        assert_eq!(
-            Err(CreationError::Negative),
-            PositiveNonzeroInteger::new(-10)
-        );
+        assert_eq!(Err(CreationError::Negative), PositiveNonzeroInteger::new(-10));
         assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
     }
 }
